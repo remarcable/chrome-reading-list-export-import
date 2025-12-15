874c4370c9e8f63c466bf757db3c4bdc2f75648f
Marc N. <git@marcnitzsche.de>
2025-12-15 11:36:24 +0100

    Cleanup code

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
     viteStaticCopy({
-      targets: [
-        {
-          src: 'public/manifest.json',
-          dest: '.',
-        }
-      ],
+      targets: [{ src: "public/manifest.json", dest: "." }],
     }),
   ],
   build: {
-    outDir: 'build',
+    outDir: "build",
     rollupOptions: {
-      input: {
-        main: './index.html',
-        background: './src/background.ts',
-      },
+      input: { main: "./index.html", background: "./src/background.ts" },
       output: {
         entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === 'background' ? 'background.js' : 'assets/[name]-[hash].js';
+          return chunkInfo.name === "background"
+            ? "background.js"
+            : "assets/[name]-[hash].js";
         },
       },
     },
